416f42142f84d9b7dfb7b9020e4ed2ea072639c0
Adrian Agnic <adrian.agnic@hotmail.com>
2017-08-18 20:45:07 -0700

    Add project files.

---
+++ TestPercep/Perceptron.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestPercep
+{
+    class Perceptron
+    {
+        private float[] weights = new float[2];
+        public float learningRate = 0.1f;
+
+        public Perceptron()
+        {
+            for (int i = 0; i < weights.Length; i++)
+            {
+                Random num = new Random();
+                weights[i] = (float)num.NextDouble();
+            }
+        }
+
+        public int Guess(float[] inputs)
+        {
+            float sum = 0;
+            for (int i = 0; i < weights.Length; i++)
+            {
+                sum += inputs[i] * weights[i];
+            }
+
+            int output = sign(sum);
+            return output;
+        }
+
+        public void Train(float[] inputs, int target)
+        {
+            int guess = Guess(inputs);
+            int error = target - guess;
+
+            for (int i = 0; i < weights.Length; i++)
+            {
+                weights[i] += error * inputs[i] * learningRate;
+            }
+        }
+
+        public float[] GetWeights()
+        {
+            return weights;
+        }
+
+        public int sign(float n)
+        {
+            if (n >= 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+    }
+}
---
+++ TestPercep/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace TestPercep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Size of Perceptron source code?");
+            string aSource = Console.ReadLine();
+            int nSource = Convert.ToInt16(aSource);
+
+
+            float[] source = new float[nSource];
+            Perceptron perceptron = new Perceptron();
+
+            Console.WriteLine("Perceptron Learning Rate? ex. 0.1");
+            string aLR = Console.ReadLine();
+            float nLR = (float)Convert.ToDouble(aLR);
+            perceptron.learningRate = nLR;
+
+            int target = -1;
+            int gen = 0;
+            bool targetHit = false;
+
+
+            for (int i = 0;i < source.Length;i++)
+            {
+                Random random = new Random();
+                float input = (float)random.NextDouble();
+                source[i] = input;
+            }
+
+
+            Console.WriteLine("Start Perceptron? (yes)(no)  Target = -1, when Target is reached weight values will not change.");
+
+            string answer = Console.ReadLine();
+
+            while (answer == "yes")
+            {
+                perceptron.Train(source, target);
+                if (targetHit == false)
+                {
+                    gen += 1;
+                }
+                float[] outputWeights = perceptron.GetWeights();
+                for (int i = 0;i < outputWeights.Length;i++)
+                {
+                    Console.WriteLine("Weight: " + outputWeights[i]);
+                }
+                int guess = perceptron.Guess(source);
+                if (guess == target)
+                {
+                    targetHit = true;
+                }
+                Console.WriteLine("Generation: " + gen);
+                Console.WriteLine("Guess: " + guess);
+                Console.WriteLine("Press Enter to train again, or reply no to stop.");
+                string response = Console.ReadLine();
+                if (response == "no")
+                {
+                    answer = response;
+                }
+            }
+        }
+    }
+}
